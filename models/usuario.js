d8e4cd493999c3a88abf7143dc709eff5a33831e
Pedro Augusto De Agostini <88171269+PedroAgostini@users.noreply.github.com>
2023-11-29 16:16:24 -0300

    Update

--- models/usuario.js
+++ models/usuario.js
@@ -12,20 +12,13 @@ const Usuarios = new Schema({
         required: true,
         unique: true
     },
+    nome:{
+        type: String,
+        required: true
+    },
     senha: String,
     chave: String
 })
-/*
-Nao utilizado
-Faz a criptografia da senha, sempre q ela for alterada ou criada.
-OBS: sem a necessidade do bcrypt em alterarsenha
-
-Usuarios.pre('save', async function(next){
-    const hash = await bcrypt.hash(this.senha, 10)
-    this.senha = hash
-    next()
-})
-*/
 
 const Usuario = mongoose.model('Usuario', Usuarios)
 module.exports = Usuario
